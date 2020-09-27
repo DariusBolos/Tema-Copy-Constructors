a7a15e2060375c745e1f6eb571e14b2989697e8c
DariusBolos <63288528+DariusBolos@users.noreply.github.com>
2020-09-27 11:08:24 +0300

    Add files via upload

---
+++ Copy_Constructors/Fraction.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Copy_Constructors
+{
+    class Fraction
+    {
+        private int _numerator;
+        private int _denominator;
+        private int _result;
+
+        public int Numerator
+        {
+            get
+            {
+                return _numerator; // sau get => _numerator
+            }
+            set
+            {
+                _numerator = value;
+            }
+        }
+
+        public int Denominator
+        {
+            get => _denominator;
+            set
+            {
+                if (value > 0)
+                    _denominator = value;
+                else
+                    throw new ArgumentNullException();
+            }
+        }
+
+        public Fraction()
+        {
+            _numerator = 0;
+            _denominator = 1;
+        }
+
+        public Fraction(Fraction copyFraction)
+        {
+            _numerator = copyFraction._numerator;
+            _denominator = copyFraction._denominator;
+        }
+    }
+}
---
+++ Copy_Constructors/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Copy_Constructors
+{
+    class Program
+    {
+ 
+        static void Main(string[] args)
+        {
+            int result1, result2, result3, result4;
+            Fraction fraction1 = new Fraction();
+            fraction1.Numerator = 10;
+            fraction1.Denominator = 2;
+
+            Fraction fraction2 = new Fraction(fraction1);
+
+            result1 = fraction2.Numerator / fraction2.Denominator;
+            result2 = fraction2.Numerator * fraction2.Denominator;
+            result3 = fraction2.Numerator + fraction2.Denominator;
+            result4 = fraction2.Numerator - fraction2.Denominator;
+
+            Console.WriteLine(fraction2.Numerator + "/" + fraction2.Denominator);
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+            Console.WriteLine(result3);
+            Console.WriteLine(result4);
+
+
+            // Tema: de gasit o modalitate pentru a afisa rezultatul (o metoda sau o variabila noua).
+        }
+    }
+}
